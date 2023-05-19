a66e471fd457f868a16f317d9886201f9bd70226
Vegard Stronen <70795294+stronenv@users.noreply.github.com>
2023-05-19 17:47:49 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -160,7 +160,7 @@ function App() {
 
           <p>We also use third party advertisements on terrassebordkalkulator.no to support our site. Some of these advertisers may use technology such as cookies and web beacons when they advertise on our site, which will also send these advertisers (such as Google through the Google AdSense program) information including your IP address, your ISP , the browser you used to visit our site, and in some cases, whether you have Flash installed.  This is generally used for geotargeting purposes (showing New York real estate ads to someone in New York, for example) or showing certain ads based on specific sites visited (such as showing cooking ads to someone who frequents cooking sites).</p>
 
-          <p>Users may opt out of personalized advertising by visiting <a href="https://www.google.com/settings/ads" target="_blank">Ads Settings</a>. (Alternatively, you can opt out of a third-party vendor's use of cookies for personalized advertising by visiting www.aboutads.info.)</p>
+          <p>Users may opt out of personalized advertising by visiting <a href="https://www.google.com/settings/ads">Ads Settings</a>. (Alternatively, you can opt out of a third-party vendor's use of cookies for personalized advertising by visiting www.aboutads.info.)</p>
 
           <p><b>DoubleClick DART cookies</b><br></br>
           We also may use DART cookies for ad serving through Google's DoubleClick, which places a cookie on your computer when you are browsing the web and visit a site using DoubleClick advertising (including some Google AdSense advertisements).  This cookie is used to serve ads specific to you and your interests (”interest based targeting”).</p>
@@ -187,7 +187,7 @@ function App() {
 
           <p>Vi bruker også tredjepartsannonser på terrassebordkalkulator.no for å støtte nettstedet vårt. Noen av disse annonsørene kan bruke teknologi som informasjonskapsler og web beacons når de annonserer på nettstedet vårt, noe som også vil sende disse annonsørene (som for eksempel Google gjennom Google AdSense-programmet) informasjon, inkludert IP-adressen din, internettleverandøren din, nettleseren du brukte for å besøke nettstedet vårt, og i noen tilfeller om du har Flash installert. Dette brukes generelt for geografisk målretting (for eksempel å vise eiendomsannonser i New York til noen i New York) eller for å vise bestemte annonser basert på spesifikke besøkte nettsteder (for eksempel å vise matlagingsannonser til noen som besøker matlagingssider ofte).</p>
 
-          <p>Brukere kan velge å deaktivere personlig tilpassede annonser ved å besøke Annonseinnstillinger. (Alternativt kan du velge å deaktivere en tredjepartsleverandørs bruk av informasjonskapsler for personlig tilpasset annonsering ved å besøke www.aboutads.info.)</p>
+          <p>Brukere kan velge å deaktivere personlig tilpassede annonser ved å besøke <a href="https://google.com/settings/ads">Annonseinnstillinger</a>. (Alternativt kan du velge å deaktivere en tredjepartsleverandørs bruk av informasjonskapsler for personlig tilpasset annonsering ved å besøke www.aboutads.info.)</p>
 
           <p><b>DoubleClick DART-informasjonskapsler</b><br></br>
           Vi kan også bruke DART-informasjonskapsler for annonsevisning gjennom Googles DoubleClick, som plasserer en informasjonskapsel på datamaskinen din når du surfer på nettet og besøker et nettsted som bruker DoubleClick-annonsering (inkludert noen Google AdSense-annonser). Denne informasjonskapselen brukes til å vise annonser som er spesifikke</p>
