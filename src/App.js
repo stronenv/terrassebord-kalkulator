6f41033813ffd94f1a97f3238ee82914b061c5ae
stronenv <70795294+stronenv@users.noreply.github.com>
2023-05-19 15:57:47 +0200

    added PP

--- src/App.js
+++ src/App.js
@@ -143,6 +143,55 @@ function App() {
           {calculatedSkruer} skruer
         </h3>
         <p><i>Kalkulatoren tar utgangspunkt i 60 cm senteravstand, 3 mm mellom hvert bord og 10% kappsvinn.</i></p>
+
+        <details>
+          <summary>Cookies og Personvernerklæring / Cookies and Privacy Policy </summary>
+          <h4>Privacy Policy for Terrassebordkalkulator.no - English</h4>
+
+          <p>The privacy of our visitors to terrassebordkalkulator.no is important to us.</p>
+
+          <p>At terrassebordkalkulator.no, we recognize that privacy of your personal information is important. Here is information on what types of personal information we receive and collect when you use and visit terrassebordkalkulator.no, and how we safeguard your information.  We never sell your personal information to third parties.</p>
+
+          <p><b>Log Files</b><br></br>
+          As with most other websites, we collect and use the data contained in log files. The information in the log files include  your IP address, your ISP, the browser you used to visit our site , the time you visited our site and which pages you visited throughout our site.</p>
+
+          <p><b>Cookies and Web Beacons</b><br></br>
+          We do use cookies to store information, such as your personal preferences when you visit our site.  This could include only showing you a popup once in your visit, or the ability to login to some of our features, such as forums.</p>
+
+          <p>We also use third party advertisements on terrassebordkalkulator.no to support our site. Some of these advertisers may use technology such as cookies and web beacons when they advertise on our site, which will also send these advertisers (such as Google through the Google AdSense program) information including your IP address, your ISP , the browser you used to visit our site, and in some cases, whether you have Flash installed.  This is generally used for geotargeting purposes (showing New York real estate ads to someone in New York, for example) or showing certain ads based on specific sites visited (such as showing cooking ads to someone who frequents cooking sites).</p>
+
+          <p>Users may opt out of personalized advertising by visiting <a href="https://www.google.com/settings/ads" target="_blank">Ads Settings</a>. (Alternatively, you can opt out of a third-party vendor's use of cookies for personalized advertising by visiting www.aboutads.info.)</p>
+
+          <p><b>DoubleClick DART cookies</b><br></br>
+          We also may use DART cookies for ad serving through Google's DoubleClick, which places a cookie on your computer when you are browsing the web and visit a site using DoubleClick advertising (including some Google AdSense advertisements).  This cookie is used to serve ads specific to you and your interests (”interest based targeting”).</p>
+
+          <p>The ads served will be targeted based on your previous browsing history (For example, if you have been viewing sites about visiting Las Vegas, you may see Las Vegas hotel advertisements when viewing a non-related site, such as on a site about hockey).  DART uses “non personally identifiable information”.  It does NOT track personal information about you, such as your name, email address, physical address, telephone number, social security numbers, bank account numbers or credit card numbers.</p>
+
+          <p>You can opt-out of this ad serving on all sites using this advertising by visiting http://www.doubleclick.com/privacy/dart_adserving.aspx</p>
+
+          <p>You can choose to disable or selectively turn off our cookies or third-party cookies in your browser settings, or by managing preferences in programs such as Norton Internet Security.  However, this can affect how you are able to interact with our site as well as other websites.  This could include the inability to login to services or programs, such as logging into forums or accounts.</p>
+
+          <p>Deleting cookies does not mean you are permanently opted out of any advertising program.  Unless you have settings that disallow cookies, the next time you visit a site running the advertisements, a new cookie will be added.</p>
+
+          <h4>Personvernerklæring for Terrassebordkalkulator.no - Norsk</h4>
+
+          <p>Personvernet til våre besøkende på terrassebordkalkulator.no er viktig for oss.</p>
+
+          <p>På terrassebordkalkulator.no erkjenner vi at personvern for din personlige informasjon er viktig. Her er informasjon om hvilke typer personlig informasjon vi samler inn og lagrer når du bruker og besøker terrassebordkalkulator.no, og hvordan vi beskytter informasjonen din. Vi selger aldri din personlige informasjon til tredjeparter.</p>
+
+          <p><b>Loggfiler</b><br></br>
+          Som de fleste andre nettsteder samler vi inn og bruker dataene som finnes i loggfiler. Informasjonen i loggfiler inkluderer din IP-adresse, din internettleverandør , nettleseren du brukte for å besøke nettstedet vårt, tidspunktet du besøkte nettstedet vårt og hvilke sider du besøkte på nettstedet vårt.</p>
+
+          <p><b>Informasjonskapsler og web beacons</b><br></br>
+          Vi bruker informasjonskapsler for å lagre informasjon, for eksempel dine personlige preferanser når du besøker nettstedet vårt. Dette kan inkludere å vise deg en popup bare én gang i løpet av besøket ditt, eller muligheten til å logge inn på noen av funksjonene våre, for eksempel forum.</p>
+
+          <p>Vi bruker også tredjepartsannonser på terrassebordkalkulator.no for å støtte nettstedet vårt. Noen av disse annonsørene kan bruke teknologi som informasjonskapsler og web beacons når de annonserer på nettstedet vårt, noe som også vil sende disse annonsørene (som for eksempel Google gjennom Google AdSense-programmet) informasjon, inkludert IP-adressen din, internettleverandøren din, nettleseren du brukte for å besøke nettstedet vårt, og i noen tilfeller om du har Flash installert. Dette brukes generelt for geografisk målretting (for eksempel å vise eiendomsannonser i New York til noen i New York) eller for å vise bestemte annonser basert på spesifikke besøkte nettsteder (for eksempel å vise matlagingsannonser til noen som besøker matlagingssider ofte).</p>
+
+          <p>Brukere kan velge å deaktivere personlig tilpassede annonser ved å besøke Annonseinnstillinger. (Alternativt kan du velge å deaktivere en tredjepartsleverandørs bruk av informasjonskapsler for personlig tilpasset annonsering ved å besøke www.aboutads.info.)</p>
+
+          <p><b>DoubleClick DART-informasjonskapsler</b><br></br>
+          Vi kan også bruke DART-informasjonskapsler for annonsevisning gjennom Googles DoubleClick, som plasserer en informasjonskapsel på datamaskinen din når du surfer på nettet og besøker et nettsted som bruker DoubleClick-annonsering (inkludert noen Google AdSense-annonser). Denne informasjonskapselen brukes til å vise annonser som er spesifikke</p>
+      </details>
       </Box>
     </Container>
   );
