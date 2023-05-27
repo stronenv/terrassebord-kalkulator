3cb805323551678afa0f3898ec116c6d3f78c032
stronenv <70795294+stronenv@users.noreply.github.com>
2023-05-27 17:38:31 +0200

    Lagt til innhold

--- src/App.js
+++ src/App.js
@@ -143,7 +143,17 @@ function App() {
           {calculatedSkruer} skruer
         </h3>
         <p><i>Kalkulatoren tar utgangspunkt i 60 cm senteravstand, 3 mm mellom hvert bord og 10% kappsvinn.</i></p>
-
+        <hr></hr>
+        <div>
+        <p>Å bygge en terrasse er en flott måte å utvide bruksområdet på utendørsområdet ditt og skape et rom for avslapning og underholdning. Før du begynner byggeprosessen, er det viktig å planlegge og beregne hvor mye materiale du trenger. Terrassebord og skruer er to essensielle elementer som må tas i betraktning.</p>
+        <h4>Beregne terassebord</h4>
+        <p>Når det gjelder terrassebord, må du først bestemme hvilken type tre eller komposittmateriale du ønsker å bruke. Vanlige trevalg inkluderer trykkimpregnert furu, sedertre eller tropiske tresorter som teak eller ipe. Komposittmaterialer består av en blanding av trefiber og plast, og de er kjent for sin holdbarhet og lave vedlikeholdsbehov. Når du har valgt materialtypen, måler du arealet på terrassen og legger inn bredde på terassebordet og kvadratmeteren i kalkulatoren for å beregne hvor mange bord du trenger. Kalkulatoren tar høyde for avkapp og erstatningsbord.</p>
+        <h4>Beregne skruer</h4>
+        <p>Når det gjelder skruer, er det viktig å velge riktig type og størrelse for å sikre at terrassebordene er festet sikkert. Rustfrie skruer er sterke og motstandsdyktige mot korrosjon, og de er vanligvis det beste valget for utendørs prosjekter. En vanlig tommelfingerregel er å bruke omtrent 30 skruer per kvadratmeter, men det er noen variasjoner. Kalkulatoren regner ut det totale antallet skruer du trenger.</p>
+        <h4>Planlegg byggeprosjektet</h4>
+        <p>Ved å planlegge og beregne mengden terrassebord og skruer du trenger, kan du unngå unødvendig sløsing og sikre en jevn byggeprosess. Husk alltid å sjekke lokale bygningsforskrifter og konsultere fagfolk hvis du er usikker på noen av beregningene eller byggestegene. Med riktig forberedelse kan du bygge en ny terrasse som vil gi deg mange års glede og bruk.</p>
+        </div>
+        <hr></hr>
         <details>
           <summary>Cookies og Personvernerklæring / Cookies and Privacy Policy </summary>
           <h4>Privacy Policy for Terrassebordkalkulator.no - English</h4>
@@ -192,6 +202,7 @@ function App() {
           <p><b>DoubleClick DART-informasjonskapsler</b><br></br>
           Vi kan også bruke DART-informasjonskapsler for annonsevisning gjennom Googles DoubleClick, som plasserer en informasjonskapsel på datamaskinen din når du surfer på nettet og besøker et nettsted som bruker DoubleClick-annonsering (inkludert noen Google AdSense-annonser). Denne informasjonskapselen brukes til å vise annonser som er spesifikke</p>
       </details>
+      <hr></hr>
       </Box>
     </Container>
   );
